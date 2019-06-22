6df61a4f4a16374675703be27da5f27369b1eb53
Taansduy <duyvt@ssg.vn>
2019-06-22 08:56:05 +0700

    suggested playlist section

---
+++ app/src/main/java/com/vinova/dotify/model/Music.kt
@@ -0,0 +1,12 @@
+package com.vinova.dotify.model
+
+class Music {
+    var id:String?=null
+    var artist:String?=null
+    var listenCounter:Int?=0
+    var musicURL:String=""
+    var name:String=""
+    var backdropURL:String?=null
+    var posterURL:String?=null
+    var description:String?=null
+}
