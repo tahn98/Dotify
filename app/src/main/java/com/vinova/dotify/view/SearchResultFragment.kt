45a4c9514774b2906bf82c1936593ca3941c738e
Taansduy <duyvt@ssg.vn>
2019-06-28 10:22:47 +0700

    final commit

--- app/src/main/java/com/vinova/dotify/view/SearchResultFragment.kt
+++ app/src/main/java/com/vinova/dotify/view/SearchResultFragment.kt
@@ -1,17 +1,16 @@
 package com.vinova.dotify.view
 
 
+import android.app.Activity
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.appcompat.widget.SearchView.*
+import android.view.inputmethod.InputMethodManager
+import androidx.appcompat.widget.SearchView.OnQueryTextListener
+import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
-
-
 import com.vinova.dotify.R
-import com.vinova.dotify.adapter.MusicAdapter
 import com.vinova.dotify.adapter.SearchResultAdapter
 import com.vinova.dotify.model.Music
 import com.vinova.dotify.utils.BaseConst
@@ -36,36 +35,53 @@ class SearchResultFragment : Fragment() {
             }
 
             override fun onQueryTextChange(query: String?): Boolean {
-                if (query != "") {
-                    query!!.toLowerCase()
-                    val result = BaseConst.musicStorage.filter {
-                        it.artist!!.toLowerCase().contains(query as CharSequence) || it.name.toLowerCase().contains(
-                            query as CharSequence
-                        )
+                if(rcv_search_result!=null)
+                {
+                    if (query != "") {
+                        query!!.toLowerCase()
+                        val result = BaseConst.musicStorage.filter {
+                            it.artist!!.toLowerCase().contains(query as CharSequence) || it.name.toLowerCase().contains(
+                                query as CharSequence
+                            )
+                        }
+                        rcv_search_result.adapter =
+                            SearchResultAdapter(context!!, result as MutableList<Music>) { music: Music ->
+                                run {
+                                    (activity as MainScreen).play(music)
+                                    hideKeyboard()
+                                }
+                            }
+                        rcv_search_result.layoutManager = LinearLayoutManager(context)
                     }
-                    rcv_search_result.adapter =
-                        SearchResultAdapter(context!!, result as MutableList<Music>) { music: Music ->
-                            run {
-                                (activity as MainScreen).play(music)
-                                (activity as MainScreen).onBackPressed()
+                    else {
+                        rcv_search_result.adapter =
+                            SearchResultAdapter(context!!, ArrayList<Music>()) { music: Music ->
+                                run {
+                                    (activity as MainScreen).play(music)
+                                    (activity as MainScreen).onBackPressed()
+                                }
                             }
-                        }
-                    rcv_search_result.layoutManager = LinearLayoutManager(context)
-                } else {
-                    rcv_search_result.adapter =
-                        SearchResultAdapter(context!!, ArrayList<Music>()) { music: Music ->
-                            run {
-                                (activity as MainScreen).play(music)
-                                (activity as MainScreen).onBackPressed()
-                            }
-                        }
-                    rcv_search_result.layoutManager = LinearLayoutManager(context)
+                        rcv_search_result.layoutManager = LinearLayoutManager(context)
+                    }
                 }
                 return true
             }
 
         })
     }
+
+    private fun hideKeyboard(): Boolean {
+        val imm = activity?.getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+        //Find the currently focused view, so we can grab the correct window token from it.
+        var view = activity?.currentFocus
+        //If no view currently has focus, create a new one, just so we can grab a window token from it
+        if (view == null) {
+            view = View(activity)
+        }
+        return imm.hideSoftInputFromWindow(view.windowToken, 0)
+    }
+
+
 
 
 }
